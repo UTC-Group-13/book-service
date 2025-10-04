e4a2efbb37c2643d1d978b09800752f247b5def0
nguyenloc710 <loc7102010@gmail.com>
2025-10-04 11:53:05 +0700

    locnh fix response AuthorResponse

--- src/main/java/org/example/bookservice/dto/response/AuthorResponse.java
+++ src/main/java/org/example/bookservice/dto/response/AuthorResponse.java
@@ -2,8 +2,14 @@ package org.example.bookservice.dto.response;
 
 import lombok.Data;
 
+import java.time.LocalDate;
+
 @Data
 public class AuthorResponse {
     private Integer id;
     private String fullName;
+    private LocalDate birthDate;
+    private String nationality;
+    private String biography;
+    private String email;
 }

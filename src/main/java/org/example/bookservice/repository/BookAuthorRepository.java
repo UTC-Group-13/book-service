a9bb1d2a1cedaf4348125aeffd5c10c016a742ce
nguyenloc710 <loc7102010@gmail.com>
2025-10-19 17:26:48 +0700

    locnh add @Modifying

--- src/main/java/org/example/bookservice/repository/BookAuthorRepository.java
+++ src/main/java/org/example/bookservice/repository/BookAuthorRepository.java
@@ -2,10 +2,11 @@ package org.example.bookservice.repository;
 
 import org.example.bookservice.entity.BookAuthor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BookAuthorRepository extends JpaRepository<BookAuthor, Integer> {
-
+    @Modifying
     void deleteByBookId(Long bookId);
 }
--- src/main/java/org/example/bookservice/repository/BookCategoryRepository.java
+++ src/main/java/org/example/bookservice/repository/BookCategoryRepository.java
@@ -2,10 +2,11 @@ package org.example.bookservice.repository;
 
 import org.example.bookservice.entity.BookCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BookCategoryRepository extends JpaRepository<BookCategory, Integer> {
-
+    @Modifying
     void deleteByBookId(Long bookId);
 }
